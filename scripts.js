fd26ac994fba43a2a12ccac01da9419e48b7282d
nelfimov <nelfimov@mail.ru>
2022-08-12 08:32:50 +0300

    Added alert when divided by 0

--- scripts.js
+++ scripts.js
@@ -61,8 +61,11 @@ document.querySelectorAll('button.calculation').forEach(button => {
 function outputToArray(string) {
   let array = string.split(' ');
   if (array[array.length - 1] === '') array.splice(array.length - 2, 2);
-  if (array.length < 3) return alert('Not enough elements');
-  return array;
+  if (array.length < 3) {
+    alert('Not enough elements');
+  } else {
+    return array;
+  }
 }
 
 function operate(array = []) {
@@ -72,59 +75,40 @@ function operate(array = []) {
   if (array.length === 3) {
     operator = array[1];
     array.splice(1, 1);
+    if (operator === '/' && parseInt(array[1]) === 0) return alert('Division by 0 is forbidden!');
     preResult = array.reduce((a, b) => mathOperator[operator](a, b));
     return output.value = preResult;
   }
   let multiplication = array.indexOf('*');
-  console.log(`Multi: ${multiplication}`);
   let division = array.indexOf('/');
-  console.log(`Div: ${division}`);
 
   // multiplcation === division only when its -1 - not in array.
   if (multiplication === division) {
-    console.log(`No multi no division`);
     preResult = array.splice(0, 3)
     operator = preResult[1];
     preResult.splice(1, 1);
     preResult = preResult.reduce((a, b) => mathOperator[operator](a, b));
-    console.log(`Preresult: ${preResult}`);
     array.splice(0, 0, preResult)
-    console.log(array);
     operate(array);
   }
   if (multiplication > -1 && multiplication < division || multiplication > -1 && division === -1) {
-    console.log(`Division`);
     preResult = array.slice(multiplication - 1, multiplication + 2);
-    console.log(`Preresult array: ${preResult}`);
     operator = preResult[1];
     preResult.splice(1, 1);
     preResult = preResult.reduce((a, b) => mathOperator[operator](a, b));
-    console.log(`Preresult: ${preResult}`);
     array.splice(multiplication - 1, 3, preResult);
-    console.log(array);
     operate(array);
   }
   if (division > -1 && division < multiplication || division > -1 && multiplication === -1) {
-    console.log(`No multi`);
     preResult = array.slice(division - 1, division + 2);
     operator = preResult[1];
     preResult.splice(1, 1);
-    if (preResult[1] == 0) return alert('Division by 0 is forbidden!');
+    if (parseInt(preResult[1]) === 0) return alert('Division by 0 is forbidden!');
     preResult = preResult.reduce((a, b) => mathOperator[operator](a, b));
-    console.log(`Preresult: ${preResult}`);
     array.splice(division - 1, 3, preResult);
-    console.log(array);
     operate(array);
   }
 }
-
-let exampleArray = ['12', '+', '7', '-', '5', '*', '3'];
-console.log(exampleArray);
-operate(exampleArray);
-
-// let exampleArray2 = ['2', '0'];
-// console.log(exampleArray2);
-// operate(exampleArray2);
 
 resultButton.addEventListener('click', () => {
   let array = outputToArray(output.value);
